2c443d687f0ae0e264e386a8fee7fa45a64f3a83
zevdvlpr <zevdvlpr@gmail.com>
2021-07-29 20:00:25 -0300

    ✨ feat: change to tsx

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid libero
+        architecto, impedit placeat fugiat culpa doloremque? Itaque quos, natus
+        rem omnis unde dolorem repudiandae laudantium tempora magnam harum nihil
+        atque!
+      </p>
+    </div>
+  );
+}
