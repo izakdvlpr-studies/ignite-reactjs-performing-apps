4540804ab21141f9acd65610d01547eea061ac6b
zevdvlpr <zevdvlpr@gmail.com>
2021-07-29 21:19:16 -0300

    ✨ feat: home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,31 @@
+import { FormEvent, useState } from 'react';
+
+import { SearchResults } from '../components/SearchResults';
+
+interface Product {
+  id: string;
+  price: number;
+  title: string;
+}
+
 export default function Home() {
+  const [search, setSearch] = useState('');
+
+  const [results, setResults] = useState<Product[]>([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
     <div>
       <h1>Hello, world!</h1>
@@ -9,6 +36,22 @@ export default function Home() {
         rem omnis unde dolorem repudiandae laudantium tempora magnam harum nihil
         atque!
       </p>
+
+      <div>
+        <h2>Search</h2>
+
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            onChange={e => setSearch(e.target.value)}
+            value={search}
+          />
+
+          <button type="submit">Buscar</button>
+        </form>
+
+        <SearchResults results={results} />
+      </div>
     </div>
   );
 }
