189d7ecb879b18144d2ea0197445832cccab34e6
zevdvlpr <zevdvlpr@gmail.com>
2021-07-30 15:42:41 -0300

    ⚙ perf: data formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,23 @@ import { SearchResults } from '../components/SearchResults';
 
 interface Product {
   id: number;
-  price: number;
   title: string;
+  price: number;
+  priceFormatted: string;
+}
+
+interface Results {
+  totalPrice: number;
+  data: Product[];
 }
 
 export default function Home() {
   const [search, setSearch] = useState('');
 
-  const [results, setResults] = useState<Product[]>([]);
+  const [results, setResults] = useState<Results>({
+    totalPrice: 0,
+    data: [],
+  });
 
   async function handleSearch(event: FormEvent) {
     event.preventDefault();
@@ -23,7 +32,25 @@ export default function Home() {
     const response = await fetch(`http://localhost:3333/products?q=${search}`);
     const data = await response.json();
 
-    setResults(data);
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+
+    const products = data.map(product => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        priceFormatted: formatter.format(product.price),
+      };
+    });
+
+    const totalPrice = data.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+
+    setResults({ totalPrice, data: products });
   }
 
   const addToWishlist = useCallback((id: number) => {
@@ -54,7 +81,11 @@ export default function Home() {
           <button type="submit">Buscar</button>
         </form>
 
-        <SearchResults results={results} onAddToWishlist={addToWishlist} />
+        <SearchResults
+          totalPrice={results.totalPrice}
+          results={results.data}
+          onAddToWishlist={addToWishlist}
+        />
       </div>
     </div>
   );
